f48c2f9e193c7a137f21a52cfa9a513bbe1e9b1a
Hongsworth <therchgaming@gmail.com>
2022-10-20 12:18:56 +0100

    Added basic find split

---
+++ Find_split_rough.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def FIND_SPLIT(x,y,room,col):
+    b_gain = 0
+    for j in range(100):
+        for i in range(len(x)):
+            if x[i][col] < -j and y[i] == room:
+                count += 1
+    v = count/2000
+    c_gain = -v * np.log2(v) - v * np.log2(v)
+    if c_gain > b_gain:   
+        b_gain = c_gain
+        b_split = j    
+
