a0fae26e167fb172f6454bc7cbc5edd9f907d789
as2620 <arthika220602@gmail.com>
2022-10-20 12:28:16 +0100

    nothing to commit

--- decision_tree.py
+++ decision_tree.py
@@ -27,17 +27,13 @@ def read_dataset(filepath):
             x.append(list(map(float, row[:-1])))
             y_labels.append(row[-1])
 
-    [classes, y] = np.unique(y_labels, return_inverse=True)
+    [classes, y] = np.unique(y_labels, return_inverse= True)
 
-    x = np.array(x)
-    y = np.array(y)
-    return (x, y, classes)
+    x = np.array(x) # contains the values of each feature 
+    y = np.array(y) # contains matching label
 
+    return (x, y, classes) # classes contains all possible labels
 
-(x, y, classes) = read_dataset("wifi_db/clean_dataset.txt")
-print(x.shape)
-print(y.shape)
-print(classes)
 
 def find_info_gain():
     # implement the calculation to find the info gain for a split
@@ -53,4 +49,12 @@ def FIND_SPLLIT():
 
 def DECISION_TREE_LEARNING():
     pass
+
+(x, y, classes) = read_dataset("wifi_db/clean_dataset.txt")
+print(x.shape)
+print(y.shape)
+print(classes) 
+
+for thing in y:
+     print(thing)
 
