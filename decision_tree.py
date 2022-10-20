e9e29057785e70bfb4e5ed991cb2a84de9f25522
Daniel <dyo20@ic.ac.uk>
2022-10-20 13:03:13 +0100

    feat: changed read_dataset to print into one array

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.random import default_rng
-
+import matplotlib.pyplot as plt
 
 def read_dataset(filepath):
     """ Read in the dataset from the specified filepath
@@ -20,23 +20,18 @@ def read_dataset(filepath):
     """
 
     x = []
-    y_labels = []
     for line in open(filepath):
         if line.strip() != "":  # handle empty rows in file
             row = line.strip().split()
-            x.append(list(map(float, row[:-1])))
-            y_labels.append(row[-1])
-
-    [classes, y] = np.unique(y_labels, return_inverse=True)
+            x.append(list(map(float, row)))
 
     x = np.array(x)
-    y = np.array(y)
-    return (x, y, classes)
+    return x
 
 
-(x, y, classes) = read_dataset("wifi_db/clean_dataset.txt")
-print(x.shape)
-print(y.shape)
-print(classes)
+x = read_dataset("wifi_db/clean_dataset.txt")
 
+
+for thing in x:
+    print(thing)
 # def decision_tree_learning():
