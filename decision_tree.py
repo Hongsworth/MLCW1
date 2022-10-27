6324e434376761c6d719a2a660a2a25d75721475
as2620 <94852307+as2620@users.noreply.github.com>
2022-10-27 17:38:14 +0100

    add evaluation metrics

--- decision_tree.py
+++ decision_tree.py
@@ -35,6 +35,8 @@ def is_leaf():
 def evaluate(test_db, trained_tree):
     pass 
 
+#________________________________________EVALUATION_METRICS__________________________________________
+
 def create_confusion_matrix(predicted_labels, actual_labels):
 
     classes = np.unique(actual_labels)
@@ -65,6 +67,51 @@ def find_accuracy(predicted_labels, actual_labels):
   accuracy = (correct_samples/total_samples) * 100 # accuracy is given as a percentage 
 
   return accuracy 
+
+def find_recall(class_num, predicted_labels, actual_labels):
+  
+  recall = 0
+  total_class_samples = 0 
+  conf_matrix = create_confusion_matrix(predicted_labels, actual_labels)
+
+  correct_class_samples = conf_matrix[class_num, class_num]
+  
+  for sample in actual_labels:
+    if sample == class_num:
+      total_class_samples += 1
+
+  recall = correct_class_samples / total_class_samples 
+
+  return recall
+  
+
+def find_precision(class_num, predicted_labels, actual_labels):
+ 
+  precision = 0 
+  total_class_samples = 0 
+  conf_matrix = create_confusion_matrix(predicted_labels, actual_labels)
+
+  correct_class_samples = conf_matrix[class_num, class_num]
+  
+  for sample in predicted_labels:
+    if sample == class_num:
+      total_class_samples += 1
+
+  precision = correct_class_samples / total_class_samples 
+
+  return precision
+
+def find_F1(class_num, predicted_labels, actual_labels):
+  
+  f_measure = 0 
+  recall = find_recall(class_num, predicted_labels, actual_labels)
+  precision = find_precision(class_num, predicted_labels, actual_labels)
+
+  f_measure = (2*recall*precision)/(precision + recall)
+
+  return f_measure
+
+#____________________________________________________BUILDING_MODEL__________________________________________________________
 
 def decision_tree_learning(dataset, depth = 0 ):
 
