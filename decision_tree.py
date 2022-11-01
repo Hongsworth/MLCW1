ef768f100f2c7c8937d2d2a0e5ac59d4ff7790df
Daniel <dyo20@ic.ac.uk>
2022-11-01 14:35:49 +0000

    chore: cleaning duplicate functions

--- decision_tree.py
+++ decision_tree.py
@@ -4,58 +4,6 @@ import numpy as np
 # from numpy.random import default_rng
 
 
-class Tree:
-    def __init__(self):
-        self.left = None
-        self.right = None
-        self.data = None
-
-
-def read_dataset(filepath):
-    x = []
-    for line in open(filepath):
-        if line.strip() != "":  # handle empty rows in file
-            row = line.strip().split()
-            x.append(list(map(float, row)))
-
-    x = np.array(x)
-    return x
-
-
-def entropy(array):
-    ones = 0
-    twos = 0
-    threes = 0
-    fours = 0
-    for element in array:
-        match element[7]:
-            case 1:
-                ones += 1
-            case 2:
-                twos += 1
-            case 3:
-                threes += 1
-            case 4:
-                fours += 1
-    return -(ones * np.log2(ones) + twos * np.log2(twos) + threes *
-             np.log2(threes) + fours * np.log2(fours))
-
-
-def information_gain(array, split):
-    main = entropy(array)
-    split1 = entropy(array[:split])
-    split2 = entropy(array[split:])
-    return main - (split1 * split + split2 * (len(array) - split)) / len(array)
-
-
-def is_leaf():
-    pass
-
-
-def evaluate(test_db, trained_tree):
-    pass
-
-
 # _____________________________EVALUATION_METRICS_______________________________
 
 def create_confusion_matrix(predicted_labels, actual_labels):
@@ -139,25 +87,3 @@ def find_F1(class_num, predicted_labels, actual_labels):
 
 
 # ____________________________BUILDING_MODEL_________________________________
-
-def decision_tree_learning(dataset, depth = 0 ):
-
-    is_leaf = is_leaf()
-
-    if is_leaf:
-        pass
-
-    else:
-        for col_num in range(0, 7):
-            best_split = find_split()
-            left_dataset, right_dataset = split_dataset()
-            node = "put smth here"
-            left_branch, left_depth = decision_tree_learning(left_dataset,
-                                                             depth + 1)
-            right_branch, right_depth = decision_tree_learning(right_dataset,
-                                                               depth + 1)
-
-            return (node, max(left_depth, right_depth))
-
-
-x = read_dataset("wifi_db/clean_dataset.txt")
